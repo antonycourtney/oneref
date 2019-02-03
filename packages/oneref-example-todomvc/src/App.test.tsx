e7cf223494e836fb5763f7d96aafb3dbc67ca597
Antony Courtney <antony.courtney@gmail.com>
2019-02-03 13:44:30 -0800

    tweak App.test to compile without errors

--- packages/oneref-example-todomvc/src/App.test.tsx
+++ packages/oneref-example-todomvc/src/App.test.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoApp from './components/TodoApp';
+import * as oneref from 'oneref';
+import TodoListEditor from './components/TodoListEditor';
+import TodoAppState from './todoAppState';
+
+import 'todomvc-common/base.css'
+import 'todomvc-app-css/index.css'
+
+const initialAppState = new TodoAppState();
+
+const TodoApp = oneref.appContainer<TodoAppState, {}>(initialAppState, TodoListEditor);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
