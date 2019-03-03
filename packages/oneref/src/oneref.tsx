ee44b9464c1ca267cc4a1246945bd600f4cad68e
Antony Courtney <antony.courtney@gmail.com>
2019-03-02 21:08:03 -0800

    Prettify oneref

--- packages/oneref/src/oneref.tsx
+++ packages/oneref/src/oneref.tsx
@@ -1,15 +1,15 @@
 /*
- * Types and utilities for using the oneref pattern for state management 
+ * Types and utilities for using the oneref pattern for state management
  */
 
 import * as React from 'react';
 import { utils } from './utils';
 
-export type StateTransformer<T> = (s: T) => T
+export type StateTransformer<T> = (s: T) => T;
 // A StateTransformer with an additional calculated value:
-export type StateTransformerAux<T, A> = (s: T) => [T, A]
+export type StateTransformerAux<T, A> = (s: T) => [T, A];
 
-export type StateUpdater<T> = (st: StateTransformer<T>) => void
+export type StateUpdater<T> = (st: StateTransformer<T>) => void;
 // This would also work:
 //   type StateUpdater<T> = React.Dispatch<React.SetStateAction<T>>
 // and is the type of setState returned from React's useState hook.
@@ -21,60 +21,74 @@ type Resolver<T> = (t: T) => void;
 
 // internal state representation used by oneRef:
 interface StateRep<T> {
-    appState: T,
+    appState: T;
     resolvers: Resolver<T>[];
-} 
+}
 
 // Ideally this should be an opaque type outside this module
-export type StateRef<T> = StateUpdater<StateRep<T>>
+export type StateRef<T> = StateUpdater<StateRep<T>>;
 
 export interface StateRefProps<T> {
-    appState: T,
-    stateRef: StateRef<T>
+    appState: T;
+    stateRef: StateRef<T>;
 }
 
 export function update<T>(ref: StateRef<T>, tf: StateTransformer<T>) {
-    ref(sr => ({ appState: tf(sr.appState), resolvers: sr.resolvers }))    
+    ref(sr => ({ appState: tf(sr.appState), resolvers: sr.resolvers }));
 }
 
-export async function awaitableUpdate<T, A>(ref: StateRef<T>, tf: StateTransformerAux<T, A>): Promise<[T, A]> {
-    return new Promise((resolve, reject) => {
+export async function awaitableUpdate<T, A>(
+    ref: StateRef<T>,
+    tf: StateTransformerAux<T, A>
+): Promise<[T, A]> {
+    return new Promise(resolve => {
         ref(sr => {
             const [appState, aux] = tf(sr.appState);
             const auxResolver = (s: T) => resolve([appState, aux]);
             const resolvers = sr.resolvers;
             resolvers.push(auxResolver);
             return { appState, resolvers };
-        })
-    })
+        });
+    });
 }
 
 // ?? TODO: add optional cleanup handler:
 // X<S> | [ X<S>, () => void]
-export type InitialStateEffect<T> = (appState: T, stateRef: StateRef<T>) => void | AsyncIterable<StateTransformer<T>>
-export type StateChangeEffect<T> = (appState: T, stateRef: StateRef<T>) => void
+export type InitialStateEffect<T> = (
+    appState: T,
+    stateRef: StateRef<T>
+) => void | AsyncIterable<StateTransformer<T>>;
+export type StateChangeEffect<T> = (appState: T, stateRef: StateRef<T>) => void;
 
-async function stStreamReader<AS>(setState: StateUpdater<AS>, stream: AsyncIterable<StateTransformer<AS>>): Promise<void> {
+async function stStreamReader<AS>(
+    setState: StateUpdater<AS>,
+    stream: AsyncIterable<StateTransformer<AS>>
+): Promise<void> {
     for await (const st of stream) {
         setState(st);
     }
 }
 
-function isAsyncIterable<S>(val: void | AsyncIterable<StateTransformer<S>>): val is AsyncIterable<StateTransformer<S>> {
-    return (typeof val !== 'undefined');
+function isAsyncIterable<S>(
+    val: void | AsyncIterable<StateTransformer<S>>
+): val is AsyncIterable<StateTransformer<S>> {
+    return typeof val !== 'undefined';
 }
 
 /*
  * A higher-order component that holds the single mutable ref cell for top-level app state
  *
  */
-export const appContainer = <AS extends {}, P extends {} = {},B = {}>(
-    as0: AS, 
+export const appContainer = <AS extends {}, P extends {} = {}, B = {}>(
+    as0: AS,
     Comp: React.ComponentType<P & StateRefProps<AS>>,
     initEffect?: InitialStateEffect<AS>,
-    onChangeEffect?: StateChangeEffect<AS>): React.FunctionComponent<P> => props => {
-
-    const [containerState, stateRef] = React.useState({ appState: as0, resolvers: [] as Resolver<AS>[] });
+    onChangeEffect?: StateChangeEffect<AS>
+): React.FunctionComponent<P> => props => {
+    const [containerState, stateRef] = React.useState({
+        appState: as0,
+        resolvers: [] as Resolver<AS>[]
+    });
 
     React.useEffect(() => {
         if (initEffect) {
@@ -82,7 +96,7 @@ export const appContainer = <AS extends {}, P extends {} = {},B = {}>(
             if (isAsyncIterable(v)) {
                 stStreamReader(tf => update(stateRef, tf), v);
             }
-        }   
+        }
     }, []);
 
     React.useEffect(() => {
@@ -96,9 +110,13 @@ export const appContainer = <AS extends {}, P extends {} = {},B = {}>(
     });
 
     return (
-        <Comp {...props} appState={containerState.appState} stateRef={stateRef} />
+        <Comp
+            {...props}
+            appState={containerState.appState}
+            stateRef={stateRef}
+        />
     );
-}
+};
 
 /*
  * Helper for state composition:
@@ -107,27 +125,39 @@ export const appContainer = <AS extends {}, P extends {} = {},B = {}>(
  * inner type from/to the outer type, return a function that can be used in a Hooks context to obtain the
  * [current state, update] functions for the inner type from the outer type and its update function.
  */
-type ProjectFunc<OT, IT> = (o: OT) => IT
-type InjectFunc<OT, IT> = (o: OT, i: IT) => OT  // i.e. functional update
-type FocusFunc<OT, IT> = (o: OT, outerRef: StateRef<OT>) => [IT, StateRef<IT>]
-export const focus =
-    <OT extends {},IT extends {}>(view: ProjectFunc<OT,IT>, inject: InjectFunc<OT,IT>): FocusFunc<OT, IT> => (o, outerRef) => {
-        /*
-         * This is a little sleazy -- we happen to know that update and awaitableUpdate are the only
-         * functions that use stateRefs, and that they are purely additive with resolvers.
-         * So we pass [] in as resolvers, and just map over any resolvers that get added to lift
-         * them from IT to OT.
-         * You are not expected to understand this; trust the types, Luke.
-         */
-        const innerStateRef: StateRef<IT> = (itf: StateTransformer<StateRep<IT>>) => {
-            outerRef(({appState, resolvers}: StateRep<OT>) => {
-                const innerRep: StateRep<IT> = { appState: view(appState), resolvers: [] };
-                const auxInnerRep = itf(innerRep);
-                const auxOuterResolvers = auxInnerRep.resolvers.map(ir => (o: OT) => ir(view(o))); 
-                return { appState: inject(appState, auxInnerRep.appState), resolvers: resolvers.concat(auxOuterResolvers)}
-            });
-        }
-        return ([view(o), innerStateRef]);
-    }
+type ProjectFunc<OT, IT> = (o: OT) => IT;
+type InjectFunc<OT, IT> = (o: OT, i: IT) => OT; // i.e. functional update
+type FocusFunc<OT, IT> = (o: OT, outerRef: StateRef<OT>) => [IT, StateRef<IT>];
+export const focus = <OT extends {}, IT extends {}>(
+    view: ProjectFunc<OT, IT>,
+    inject: InjectFunc<OT, IT>
+): FocusFunc<OT, IT> => (o, outerRef) => {
+    /*
+     * This is a little sleazy -- we happen to know that update and awaitableUpdate are the only
+     * functions that use stateRefs, and that they are purely additive with resolvers.
+     * So we pass [] in as resolvers, and just map over any resolvers that get added to lift
+     * them from IT to OT.
+     * You are not expected to understand this; trust the types, Luke.
+     */
+    const innerStateRef: StateRef<IT> = (
+        itf: StateTransformer<StateRep<IT>>
+    ) => {
+        outerRef(({ appState, resolvers }: StateRep<OT>) => {
+            const innerRep: StateRep<IT> = {
+                appState: view(appState),
+                resolvers: []
+            };
+            const auxInnerRep = itf(innerRep);
+            const auxOuterResolvers = auxInnerRep.resolvers.map(ir => (o: OT) =>
+                ir(view(o))
+            );
+            return {
+                appState: inject(appState, auxInnerRep.appState),
+                resolvers: resolvers.concat(auxOuterResolvers)
+            };
+        });
+    };
+    return [view(o), innerStateRef];
+};
 
-export {utils as utils} from './utils';
+export { utils } from './utils';
--- packages/oneref/src/utils.ts
+++ packages/oneref/src/utils.ts
@@ -2,15 +2,16 @@
  * some generic utilities that are useful in conjunction with oneref
  */
 namespace utils {
+  // A generic routine to turn a publisher type interface into an async iterator:
+  export type Listener<T> = (v: T) => void;
+  export type Publisher<T> = (l: Listener<T>) => void;
 
-// A generic routine to turn a publisher type interface into an async iterator:
-export type Listener<T> = (v: T) => void;
-export type Publisher<T> = (l: Listener<T>) => void;
+  // Type of a resolve function for a promise:
+  type Resolver<T> = (v?: T) => void;
 
-// Type of a resolve function for a promise:
-type Resolver<T> = (v?: T) => void;
-
-export async function* publisherAsyncIterable<T>(subscribe: Publisher<T>): AsyncIterable<T> {
+  export async function* publisherAsyncIterable<T>(
+    subscribe: Publisher<T>
+  ): AsyncIterable<T> {
     // We'll maintain a queue of promises that always has at least one entry.
     // The iterator will pop the next promise off the queue, await the result and yield that value.
     // The listener will resolve the last promise in the queue and append a Promise to be used
@@ -18,39 +19,42 @@ export async function* publisherAsyncIterable<T>(subscribe: Publisher<T>): Async
     let nextResolver: Resolver<T> | null = null;
     let queue: Promise<T>[] = [];
     const pushNext = () => {
-        queue.push(new Promise((resolve, reject) => {
-            nextResolver = resolve;
-        }));
-    }
+      queue.push(
+        new Promise((resolve, reject) => {
+          nextResolver = resolve;
+        })
+      );
+    };
 
     pushNext();
-    subscribe((v) => {
-        nextResolver!(v);   // should never be null
-        pushNext();
+    subscribe(v => {
+      nextResolver!(v); // should never be null
+      pushNext();
     });
 
     // And now the iterator logic:
     while (true) {
-        const v = await queue.shift()!;
-        yield v;
+      const v = await queue.shift()!;
+      yield v;
     }
-}
+  }
 
-// 'map' over an async iterable:
-export async function* aiMap<A, B>(src: AsyncIterable<A>, f: (a: A) => B): AsyncIterable<B> {
+  // 'map' over an async iterable:
+  export async function* aiMap<A, B>(
+    src: AsyncIterable<A>,
+    f: (a: A) => B
+  ): AsyncIterable<B> {
     for await (const a of src) {
-        yield f(a);
+      yield f(a);
     }
-}
+  }
 
-export async function delay(waitTime: number): Promise<void> {
+  export async function delay(waitTime: number): Promise<void> {
     const p = new Promise<void>((resolve, reject) => {
-        setTimeout(resolve, waitTime);
+      setTimeout(resolve, waitTime);
     });
     return p;
-}
-
-
+  }
 } // namespace
 
 export { utils };
